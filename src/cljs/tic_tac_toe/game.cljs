68688bceea4f39dd611e0215dd9101fedb069782
Dan Ilin <dan.ilin@me.com>
2016-01-21 00:03:05 -0500

    Add get-marker helper function

--- src/cljs/tic_tac_toe/game.cljs
+++ src/cljs/tic_tac_toe/game.cljs
@@ -4,14 +4,17 @@
   {:player true
    :board  (vec (doall (repeat n (vec (doall (repeat n nil))))))})
 
+(defn get-marker [game]
+  (case (:player game)
+    false "O"
+    true "X"))
+
 (defn get-val [game x y]
   (nth (nth (:board game) y) x " "))
 
 (defn make-move [game x y]
-  (let [board (:board game)
-        player (:player game)]
+  (let [board (:board game)]
     (if (nil? (get-val game x y))
-      {:player (not player)
-       :board  (assoc board y (assoc (get board y) x (case player false "o"
-                                                                  true "x")))}
+      {:player (not (:player game))
+       :board  (assoc board y (assoc (get board y) x (get-marker game)))}
       game)))
