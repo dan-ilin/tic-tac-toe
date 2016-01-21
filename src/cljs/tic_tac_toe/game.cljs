0fc797cfb9bb430a6e39344ba273650ba30adb10
Dan Ilin <dan.ilin@me.com>
2016-01-21 00:06:32 -0500

    Refactor

--- src/cljs/tic_tac_toe/game.cljs
+++ src/cljs/tic_tac_toe/game.cljs
@@ -1,20 +1,21 @@
 (ns tic-tac-toe.game)
 
 (defn init [n]
-  {:player true
+  {:player "X"
    :board  (vec (doall (repeat n (vec (doall (repeat n nil))))))})
-
-(defn get-marker [game]
-  (case (:player game)
-    false "O"
-    true "X"))
 
 (defn get-val [game x y]
   (nth (nth (:board game) y) x " "))
 
+(defn update-player [player]
+  (case player
+    "X" "O"
+    "O" "X"))
+
 (defn make-move [game x y]
-  (let [board (:board game)]
+  (let [board (:board game)
+        player (:player game)]
     (if (nil? (get-val game x y))
-      {:player (not (:player game))
-       :board  (assoc board y (assoc (get board y) x (get-marker game)))}
+      {:player (update-player player)
+       :board  (assoc board y (assoc (get board y) x player))}
       game)))
